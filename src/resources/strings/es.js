4f9e6a6dcb70491d7bf9d92dc94d060e90e9c37a
Maria  Gutierrez <mgutierrezc@ucenfotec.ac.cr>
2017-07-17 00:26:58 -0600

    fixing issues

---
+++ src/resources/strings/es.js
@@ -0,0 +1,19 @@
+// Group keys by module or section
+export default {
+  titles:{
+    provinces: 'Provincias',
+    ticoStops: 'Tico Stops',
+    touristictInterest: 'Interés turístico',
+    touristictDestination: 'Destino turístico'
+  },
+  provinceInfo: {
+    history: 'Historia:',
+    culture: 'Cultura:',
+    cantons: 'Cantones',
+    noContacts: 'No hay contactos',
+    tabs: {
+      info: 'Información',
+      contacts: 'Contactos emergencia'
+    }
+  }
+};
