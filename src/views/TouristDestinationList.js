b8b6e87434a49896bc892c64ef7cfb4e2a9874b6
Juan Carlos Sánchez Guzmán <jcarlos0305@hotmail.com>
2017-08-03 00:34:02 -0600

    Sprint 5

--- src/views/TouristDestinationList.js
+++ src/views/TouristDestinationList.js
@@ -1,10 +1,25 @@
-import React, { Component } from 'react';
-import { TouchableOpacity, Animated, View, ScrollView, FlatList, Image } from 'react-native';
-import { Actions } from 'react-native-router-flux';
-import { connect } from 'react-redux';
+import React, {Component} from 'react';
+import {TouchableOpacity, View, ScrollView, Image} from 'react-native';
+import {Actions} from 'react-native-router-flux';
+import {connect} from 'react-redux';
 import I18n from '../services/languageService';
-import MapView from 'react-native-maps';
-import { Container, Tabs, Tab, Text, Content, Header, Title, Input, Icon, List, ListItem, Grid, Row, Body, Button, Item, DeckSwiper, Card, CardItem, Left, Thumbnail } from 'native-base';
+// import MapView from 'react-native-maps';
+import {
+  Container,
+  Tabs,
+  Tab,
+  Text,
+  Content,
+  Header,
+  Input,
+  Icon,
+  Row,
+  Button,
+  Item,
+  Card,
+  CardItem,
+  Left
+} from 'native-base';
 
 class TouristDestinationList extends Component {
 
@@ -17,34 +32,41 @@ class TouristDestinationList extends Component {
         latitude: 37.78825,
         longitude: -122.4324,
         latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
+        longitudeDelta: 0.0421
       }
     };
   }
 
-  componentWillMount(){
-    this.props.attributes.forEach(x => this.props.attributes.push(x));
-    this.props.attributes.forEach(x => this.props.attributes.push(x));
-    this.props.attributes.forEach(x => this.props.attributes.push(x));
-
-    this.props.touristDestinations.forEach(x => this.props.touristDestinations.push(x));
-    this.props.touristDestinations.forEach(x => this.props.touristDestinations.push(x));
-    this.props.touristDestinations.forEach(x => this.props.touristDestinations.push(x));
-  }
-
   renderTouristDestinations() {
+
     return this.props.touristDestinations.map((touristDest, indx) => {
       return (
-        <Card key={indx} style={{ width: 160, height: 240 }}>
-          <CardItem cardBody>
-           <Image style={{ flex: 1, height: 150, margin: 5 }} source={{uri: touristDest.photos[0]}} />
-         </CardItem>
-         <CardItem>
-           <Left>
-             <Text style={{textAlign: 'center', flex: 1}}>{touristDest.name}</Text>
-           </Left>
-         </CardItem>
-        </Card>
+        <TouchableOpacity key={indx} onPress={() => Actions.touristDestionation({title: touristDest.name, touristDest: touristDest})}>
+          <Card style={{
+            width: 160,
+            height: 240
+          }}>
+            <CardItem cardBody>
+              <Image
+                style={{
+                  flex: 1,
+                  height: 150,
+                  margin: 5
+                }}
+                source={{
+                  uri: touristDest.photos[0].url
+                }}/>
+            </CardItem>
+            <CardItem>
+              <Left>
+                <Text style={{
+                  textAlign: 'center',
+                  flex: 1
+                }}>{touristDest.name}</Text>
+              </Left>
+            </CardItem>
+          </Card>
+        </TouchableOpacity>
       );
     });
   }
@@ -56,16 +78,30 @@ class TouristDestinationList extends Component {
 
   getAdvancedSearchBar() {
     return (
-      <View style={{flex: 1, height: 175}}>
-        <ScrollView style={{flex: 1, height: 175}}>
-          <View style={{flexDirection: 'row', flexWrap: 'wrap', flex: 1}}>
+      <View style={{
+        flex: 1,
+        height: 175
+      }}>
+        <ScrollView style={{
+          flex: 1,
+          height: 175
+        }}>
+          <View style={{
+            flexDirection: 'row',
+            flexWrap: 'wrap',
+            flex: 1
+          }}>
             {this.props.attributes.map((attr, indx) => {
               if (this.state.tags.indexOf(attr) === -1) {
-                return <Button style={{margin: 5}} key={indx} rounded small light onPress={() => this.tagToSearch(attr)}>
+                return <Button style={{
+                  margin: 5
+                }} key={indx} rounded small light onPress={() => this.tagToSearch(attr)}>
                   <Text>{attr.name}</Text>
                 </Button>;
               } else {
-                return <Button style={{margin: 5}} key={indx} rounded small success onPress={() => this.tagToSearch(attr)}>
+                return <Button style={{
+                  margin: 5
+                }} key={indx} rounded small success onPress={() => this.tagToSearch(attr)}>
                   <Text>{attr.name}</Text>
                 </Button>;
               }
@@ -79,8 +115,7 @@ class TouristDestinationList extends Component {
   tagToSearch(tag) {
     let indx = this.state.tags.indexOf(tag);
 
-    if (indx !== -1)
-    {
+    if (indx !== -1) {
       this.state.tags.splice(indx, 1);
     } else {
       this.state.tags.push(tag);
@@ -91,7 +126,7 @@ class TouristDestinationList extends Component {
 
   onRegionChange(region) {
     console.log(region);
-    this.setState({ region });
+    this.setState({region});
   }
 
   render() {
@@ -99,10 +134,12 @@ class TouristDestinationList extends Component {
       <Container>
         <Header searchBar rounded>
           <Item>
-            <Icon name="ios-search" />
-            <Input placeholder={I18n.t('general.search')} />
+            <Icon name="ios-search"/>
+            <Input placeholder={I18n.t('general.search')}/>
             <TouchableOpacity onPress={() => this.changeAdvancedBarVisibility()}>
-              <Icon name={this.state.showAdvancedSearchBar === false ? 'ios-arrow-down' : 'ios-arrow-up'} />
+              <Icon name={this.state.showAdvancedSearchBar === false
+                ? 'ios-arrow-down'
+                : 'ios-arrow-up'}/>
             </TouchableOpacity>
           </Item>
           <Button transparent>
@@ -112,7 +149,9 @@ class TouristDestinationList extends Component {
         <Content>
           {this.state.showAdvancedSearchBar && this.getAdvancedSearchBar()}
 
-          <Tabs initialPage={0} style={{flex:1}}>
+          <Tabs initialPage={0} style={{
+            flex: 1
+          }}>
             <Tab heading={I18n.t('touristDestionation.tabDestinations')}>
               <Row>
                 {/*
@@ -137,7 +176,6 @@ class TouristDestinationList extends Component {
               </Row>
             </Tab>
 
-
           </Tabs>
         </Content>
       </Container>
@@ -146,11 +184,7 @@ class TouristDestinationList extends Component {
 }
 
 const mapStateToProps = state => {
-  return {
-    touristDestinations: state.db.staticData.touristDestinations,
-    provinces: state.db.staticData.provinces,
-    attributes: state.db.staticData.attributes
-  };
+  return {touristDestinations: state.db.staticData.touristDestinations, provinces: state.db.staticData.provinces, attributes: state.db.staticData.attributes};
 };
 
 export default connect(mapStateToProps, null)(TouristDestinationList);
